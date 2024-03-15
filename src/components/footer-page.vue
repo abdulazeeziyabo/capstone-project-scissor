8e02ac12dc411284afe0473232de62d014f7bf45
iyabo abdulazeez <bawallabolaji@gmail.com>
2024-03-15 00:25:07 -0700

    gap added

--- src/components/footer-page.vue
+++ src/components/footer-page.vue
@@ -1,32 +1,33 @@
 <template>
-    <div>
-    <div>
-        <div class="mt-[200px] w-[544px] flex">
-      <div class="flex flex-col gap-6 items-center">
-        <div class="">
-          <img src="../assets/images/Logo.png">
-        </div>
-        <div class="flex gap-3 items-center">
-          <a href="mail:bawallabolaji@gmail.com"><img src="../assets/images/social-facebook.svg" alt=" FaceBook Logo" class="w-[54px] h-[54px] cursor-pointer"></a>
-          <a href="mail:bawallabolaji@gmail.com"><img src="../assets/images/linkedin-logo.svg" alt="LinkedIn Logo" class="w-[54px] h-[54px] cursor-pointer" ></a>
-          <a href="mail:bawallabolaji@gmail.com"><img src="../assets/images/twitter.svg" alt="X Logo" class="w-[54px] h-[54px]"></a>
-        </div>
-        </div>
+  <div class="flex justify-between -ml-[600px] gap-[200px] items-center">
+    <div class="">
+      <img src="../assets/images/Logo.png" />
+      <div class="flex gap-3 items-center">
+      <a href="mail:bawallabolaji@gmail.com"
+        ><img
+          src="../assets/images/social-facebook.svg"
+          alt=" FaceBook Logo"
+          class="w-[54px] h-[54px] cursor-pointer"
+      /></a>
+      <a href="mail:bawallabolaji@gmail.com"
+        ><img
+          src="../assets/images/linkedin-logo.svg"
+          alt="LinkedIn Logo"
+          class="w-[54px] h-[54px] cursor-pointer"
+      /></a>
+      <a href="mail:bawallabolaji@gmail.com"
+        ><img src="../assets/images/twitter.svg" alt="X Logo" class="w-[54px] h-[54px]"
+      /></a>
     </div>
     </div>
-  <div>
-    <div class="flex gap-3 mb-4 justify-end px-5">
-        <p class="text-end ">Created by Abdulazeez Iyabo</p>
-        <p class="border-l-2 border-[#5C6F7F] pl-4">Scissor 2024</p>
-       
+    
+    <div class="flex gap-3 mb-4 px-5">
+      <p class="text-end">Created by Abdulazeez Iyabo</p>
+      <p class=" border-[#5C6F7F] pl-4">Scissor 2024</p>
     </div>
   </div>
-    </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
